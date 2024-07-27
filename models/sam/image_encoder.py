825476edcb5d363699fbcf48dcebfeb838ff33ae
TouHouE <hsu.william9@gmail.com>
2024-07-27 16:39:52 +0800

    refactor: unable debug hint

--- models/sam/image_encoder.py
+++ models/sam/image_encoder.py
@@ -103,7 +103,7 @@ class ImageEncoderViT(nn.Module):
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ic(x.shape)
+        # ic(x.shape)
         x = self.patch_embed(x)
         if self.pos_embed is not None:
             x = x + self.pos_embed
