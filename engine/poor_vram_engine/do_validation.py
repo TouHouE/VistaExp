2e3e16944a3a625101a6409b4b9df6f0dca42358
Hsu-Ntut <t112368038@ntut.org.tw>
2024-10-05 01:12:43 +0800

    fix: let program workable

--- engine/poor_vram_engine/do_validation.py
+++ engine/poor_vram_engine/do_validation.py
@@ -42,26 +42,32 @@ def iter_slice_patch(
     for adpt_pseudo_bs, slice_idx in zip(map(len, seq_slice_ids), seq_slice_ids):
         step_cnt += adpt_pseudo_bs
         inputs, labels = inputs_l[slice_idx], labels_l[slice_idx]
-        data, target, target_original, skip = ModelInputer.prepare_sam_training_input(
-            inputs.cuda(args.rank), labels.cuda(args.rank), args, model
+        data, target, _ = ModelInputer.prepare_sam_val_input_cp_only(
+            inputs.cuda(args.rank), labels.cuda(args.rank), args
         )
         with autocast(enabled=args.amp):
             outputs = model(data, is_train=False)
 
             # Let shape from N_class_propmt x B x H x W -> B x N_class_prompt x H x W
+
         if adpt_pseudo_bs > 1:
-            pred_mask = torch.cat([_pack['low_res_logits'].permute(1, 0, 2, 3) for _pack in outputs], dim=0)
+            pred_mask = torch.cat([_pack['high_res_logits'].permute(1, 0, 2, 3) for _pack in outputs], dim=0)
         else:
-            pred_mask = outputs[0]['low_res_logits'].permute(1, 0, 3, 4).contiguous()
+            pred_mask = outputs[0]['high_res_logits'].permute(1, 0, 2, 3).contiguous()
+
         pred_mask = torch.stack(post_pred(decollate_batch(pred_mask)), dim=1)
-        pred_record.append(pred_mask.cpu().item())
-        target_record.append(target.cpu().item())
+        pred_record.append(pred_mask.cpu())
+        target_record.append(target.cpu())
 
         del inputs, labels, pred_mask, target
         gc.collect()
         torch.cuda.empty_cache()
-    quasi3d_pred = torch.cat(pred_record, dim=0)[None].permute(0, 2, 1, 3, 4)  # (B=1) x N_class x N_patch x H x W
+
+    # pred shape is: N_class x N_patch x H x W
+    quasi3d_pred = torch.cat(pred_record, dim=1)[None]  # (B=1) x N_class x N_patch x H x W
+    # target shape is: N_patch x N_class x H x W -> (B=1) x N_class x N_patch x H x W
     quasi3d_label = torch.cat(target_record, dim=0)[None].permute(0, 2, 1, 3, 4)  # the shape like quasi3d_pred
+    breakpoint()
     sampling_acc = compute_dice(quasi3d_pred, quasi3d_label)
     args.quasi_batch_size = pseudo_bs
     return sampling_acc
@@ -74,7 +80,7 @@ def val_epoch(
         post_label: Optional[Any] = None, post_pred=None, **kwargs
 ) -> AverageMeter:
     model.eval()
-    run_acc = AverageMeter()
+    run_acc = AverageMeter(args=args)
     start_time = time.time()
     n_slice = args.roi_z_iter
     hf_slice = n_slice // 2
@@ -102,7 +108,8 @@ def val_epoch(
         class_dice = adpt_all_patch(
             val_patch_ids, inputs_l, labels_l, model, args=args, post_pred=post_pred
         )
-
+        not_nans = args.nc - 1 - torch.isnan(class_dice).float().sum()
+        acc = torch.nansum(class_dice) / not_nans
         print(f"Rank: {args.rank}, Case: {f_name}, Acc: {acc:.4f}, N_prompts: {int(not_nans)} ")
         acc = torch.tensor(acc).cuda(args.rank)
         not_nans = torch.tensor(not_nans).cuda(args.rank)
@@ -116,6 +123,7 @@ def val_epoch(
                 run_acc.update_dice(class_dice)
         else:
             run_acc.update(acc.cpu().numpy(), n=not_nans.cpu().numpy())
+            run_acc.update_dice(class_dice)
         Terminate.show_validing_info(epoch, run_acc.avg, idx, len(loader), start_time, args)
         start_time = time.time()
     run_acc.log_worst(kwargs.get('run'), epoch)
